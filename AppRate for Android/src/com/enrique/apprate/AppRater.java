171f3d30a8c09409eada5f8d9ff8ff08ade7ff63
TimotheeJeannin <timojeajea@gmail.com>
2012-08-20 13:46:34 +0200

    Formatting.

--- AppRate for Android/src/com/enrique/apprate/AppRater.java
+++ AppRate for Android/src/com/enrique/apprate/AppRater.java
@@ -12,91 +12,97 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class AppRater {
-	
-    private static final String PREF_DATE_FIRST_LAUNCH = "date_firstlaunch";
+
+	private static final String PREF_DATE_FIRST_LAUNCH = "date_firstlaunch";
 	private static final String PREF_LAUNCH_COUNT = "launch_count";
 	private static final String PREF_DONT_SHOW_AGAIN = "dontshowagain";
-    
+
 	private final static String APP_TITLE = "YOUR-APP-NAME";
-    private final static String APP_PNAME = "YOUR-PACKAGE-NAME";
-    
-    private final static int DAYS_UNTIL_PROMPT = 3;
-    private final static int LAUNCHES_UNTIL_PROMPT = 7;
-    
-    public static void init(Context mContext) {
-        SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
-        if (prefs.getBoolean(PREF_DONT_SHOW_AGAIN, false)) { return ; }
-        
-        SharedPreferences.Editor editor = prefs.edit();
-        
-        // Increment launch counter
-        long launch_count = prefs.getLong(PREF_LAUNCH_COUNT, 0) + 1;
-        editor.putLong(PREF_LAUNCH_COUNT, launch_count);
+	private final static String APP_PNAME = "YOUR-PACKAGE-NAME";
 
-        // Get date of first launch
-        Long date_firstLaunch = prefs.getLong(PREF_DATE_FIRST_LAUNCH, 0);
-        if (date_firstLaunch == 0) {
-            date_firstLaunch = System.currentTimeMillis();
-            editor.putLong(PREF_DATE_FIRST_LAUNCH, date_firstLaunch);
-        }
-        
-        // Wait at least n days before opening
-        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
-            if (System.currentTimeMillis() >= date_firstLaunch + 
-                    (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
-                showRateDialog(mContext, editor);
-            }
-        }
-        
-        editor.commit();
-    }   
-    
-    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
-        final Dialog dialog = new Dialog(mContext);
-        dialog.setTitle("Rate " + APP_TITLE);
+	private final static int DAYS_UNTIL_PROMPT = 3;
+	private final static int LAUNCHES_UNTIL_PROMPT = 7;
 
-        LinearLayout ll = new LinearLayout(mContext);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        
-        TextView tv = new TextView(mContext);
-        tv.setText("If you enjoy using " + APP_TITLE + ", please take a moment to rate it. Thanks for your support!");
-        tv.setWidth(240);
-        tv.setPadding(4, 0, 4, 10);
-        ll.addView(tv);
-        
-        Button b1 = new Button(mContext);
-        b1.setText("Rate " + APP_TITLE);
-        b1.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + APP_PNAME)));
-                dialog.dismiss();
-            }
-        });        
-        ll.addView(b1);
+	public static void init(Context mContext) {
 
-        Button b2 = new Button(mContext);
-        b2.setText("Remind me later");
-        b2.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-        ll.addView(b2);
+		SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
+		if (prefs.getBoolean(PREF_DONT_SHOW_AGAIN, false)) {
+			return;
+		}
 
-        Button b3 = new Button(mContext);
-        b3.setText("No, thanks");
-        b3.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if (editor != null) {
-                    editor.putBoolean(PREF_DONT_SHOW_AGAIN, true);
-                    editor.commit();
-                }
-                dialog.dismiss();
-            }
-        });
-        ll.addView(b3);
+		SharedPreferences.Editor editor = prefs.edit();
 
-        dialog.setContentView(ll);        
-        dialog.show();        
-    }
+		// Increment launch counter
+		long launch_count = prefs.getLong(PREF_LAUNCH_COUNT, 0) + 1;
+		editor.putLong(PREF_LAUNCH_COUNT, launch_count);
+
+		// Get date of first launch
+		Long date_firstLaunch = prefs.getLong(PREF_DATE_FIRST_LAUNCH, 0);
+		if (date_firstLaunch == 0) {
+			date_firstLaunch = System.currentTimeMillis();
+			editor.putLong(PREF_DATE_FIRST_LAUNCH, date_firstLaunch);
+		}
+
+		// Wait at least n days before opening
+		if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
+			if (System.currentTimeMillis() >= date_firstLaunch
+					+ (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
+				showRateDialog(mContext, editor);
+			}
+		}
+
+		editor.commit();
+	}
+
+	public static void showRateDialog(final Context mContext,
+			final SharedPreferences.Editor editor) {
+		final Dialog dialog = new Dialog(mContext);
+		dialog.setTitle("Rate " + APP_TITLE);
+
+		LinearLayout ll = new LinearLayout(mContext);
+		ll.setOrientation(LinearLayout.VERTICAL);
+
+		TextView tv = new TextView(mContext);
+		tv.setText("If you enjoy using " + APP_TITLE
+				+ ", please take a moment to rate it. Thanks for your support!");
+		tv.setWidth(240);
+		tv.setPadding(4, 0, 4, 10);
+		ll.addView(tv);
+
+		Button b1 = new Button(mContext);
+		b1.setText("Rate " + APP_TITLE);
+		b1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+						.parse("market://details?id=" + APP_PNAME)));
+				dialog.dismiss();
+			}
+		});
+		ll.addView(b1);
+
+		Button b2 = new Button(mContext);
+		b2.setText("Remind me later");
+		b2.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		ll.addView(b2);
+
+		Button b3 = new Button(mContext);
+		b3.setText("No, thanks");
+		b3.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (editor != null) {
+					editor.putBoolean(PREF_DONT_SHOW_AGAIN, true);
+					editor.commit();
+				}
+				dialog.dismiss();
+			}
+		});
+		ll.addView(b3);
+
+		dialog.setContentView(ll);
+		dialog.show();
+	}
 }
