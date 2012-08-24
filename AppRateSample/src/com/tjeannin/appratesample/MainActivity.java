6d20da2ab864b27439380647a409bf11d7212773
Timothée Jeannin <timojeajea@gmail.com>
2012-08-24 21:39:35 +0200

    Updated sample interface.

--- AppRateSample/src/com/tjeannin/appratesample/MainActivity.java
+++ AppRateSample/src/com/tjeannin/appratesample/MainActivity.java
@@ -2,20 +2,33 @@ package com.tjeannin.appratesample;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.tjeannin.apprate.AppRater;
 
 public class MainActivity extends Activity implements OnClickListener {
 
+	private static final String LUNCH_COUNT = "lunch_count";
+	private int lunchCount;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		lunchCount = sharedPreferences.getInt(LUNCH_COUNT, 0) + 1;
+		sharedPreferences.edit().putInt(LUNCH_COUNT, lunchCount).commit();
+
 		setContentView(R.layout.activity_main);
+
+		((TextView) findViewById(R.id.lunch_count)).setText(String.valueOf(lunchCount));
 		findViewById(R.id.restart_activity).setOnClickListener(this);
+		findViewById(R.id.force_crash).setOnClickListener(this);
 		findViewById(R.id.reset_apprate_prefs).setOnClickListener(this);
 
 		// Init AppRater.
@@ -25,6 +38,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				.init();
 	}
 
+	@SuppressWarnings("null")
 	@Override
 	public void onClick(View view) {
 
@@ -36,6 +50,12 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		case R.id.reset_apprate_prefs:
 			AppRater.reset(this);
+			break;
+
+		case R.id.force_crash:
+			String crash = null;
+			crash.toString();
+			break;
 
 		default:
 			break;
